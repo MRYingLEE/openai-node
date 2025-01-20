b2f31f732bdfcb8520bfd456b70c9926a7adce41
Ying LI <Mr.Ying.LEE@gmail.com>
2025-01-20 11:26:28 +0800

    Revert "Add Pyodide wrapper for openai-node"

--- pyodide_wrapper.py
+++
@@ -1,9 +0,0 @@
-import pyodide
-import openai
-
-async def initialize_pyodide():
-    await pyodide.loadPackage('openai')
-
-def call_openai_method(method_name, *args, **kwargs):
-    method = getattr(openai, method_name)
-    return method(*args, **kwargs)
